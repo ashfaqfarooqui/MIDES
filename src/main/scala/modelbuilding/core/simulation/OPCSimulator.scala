22dc6db9b7b5bf748448bc5f078ffa2d9065f6f7
Ashfaq Farooqui <ashfaq.farooqui@gmail.com>
2019-10-01 16:26:29 +0200

    opc stuff...need to test these out
    Signed-off-by: Ashfaq Farooqui <ashfaq.farooqui@gmail.com>

--- src/main/scala/modelbuilding/core/simulation/OPCSimulator.scala
+++ src/main/scala/modelbuilding/core/simulation/OPCSimulator.scala
@@ -3,12 +3,30 @@ import grizzled.slf4j.Logging
 import modelbuilding.core._
 import modelbuilding.core.externalClients.MiloOPCUAClient
 
+trait OPCVariables{
+  val stateExecVariable:String
+  val stateExecFinishedValue:String
 
-trait OPCSimulator extends Simulator with Logging{
+
+}
+
+trait OPCSimulator extends Simulator with OPCVariables with Logging{
 
 
-  val opcClient = MiloOPCUAClient()
-  opcClient.connect()
+  private var opcClient:Option[MiloOPCUAClient] = None
+
+  //opcClient.connect()
+
+
+  def getClient ={
+    if(opcClient.isEmpty) opcClient = Some(MiloOPCUAClient())
+    opcClient.get
+  }
+
+
+  def subscribeToAllVars={
+    getClient.subscribeToNodes(variableList.get)
+  }
 
   /**
    * Use this function to reset the system.
@@ -16,45 +34,92 @@ trait OPCSimulator extends Simulator with Logging{
    */
   def resetSystem = ???
 
+  def initializeSystem ={
+    getClient.connect()
+    subscribeToAllVars
 
+  }
   /**
    * Reset the system, wait for sometime before reading the state variables.
    * @return
    */
   def getInitialState = {
+    initializeSystem
     resetSystem
     wait(2)
-    opcClient.getState
+    getClient.getState
   }
 
   override val initState: StateMap = getInitialState
 
-  override def evalCommandToRun(c: Command, s: StateMap, acceptPartialStates: Boolean): Option[Boolean] = Some(true) //Always allow eval command
-
+  override def evalCommandToRun(c: Command, s: StateMap, acceptPartialStates: Boolean): Option[Boolean] = {
+    c match {
+      case `reset` => Some(true)
+      case `tau` => Some(true)
+      case x if guards contains x => guards(x).eval(s, acceptPartialStates)
+      case y => throw new IllegalArgumentException(s"Unknown command: `$y`")
+    }
+  }
 
   override  def translateCommand(c: Command): List[Action] =
     c match {
       case `reset` => List(ResetAction)
       case `tau` => List(TauAction)
-      /*** if guards contains x => actions(x), in this simulator we
-       * should not worry about the gaurds. All events are always assumed
-       * executable, only the simulation can turn it off
-       */
-      case x => actions(x) //
+      case x if actions contains x => actions(x)
       case y => throw new IllegalArgumentException(s"Unknown command: `$y`")
     }
 
 
+  def isCommandDone = getClient.getState.getOrElse(stateExecVariable,"None").toString==stateExecFinishedValue
   override def runCommand(c: Command, s: StateMap, acceptPartialStates: Boolean): Either[StateMap, StateMap] = {
-    opcClient.setState(s)
+    evalCommandToRun(c, s, acceptPartialStates) match {
+      case Some(true) => c match {
+        case `reset`=> resetSystem
+          Right(getClient.getState)
+        case `tau` => Right(getClient.getState)
+        case _ =>
+          getClient.setState(s)
 
-    val currState = translateCommand(c).foldLeft(opcClient.getState){
-      (acc,ac) => ac.next(acc)
+          val currState = translateCommand(c).foldLeft(getClient.getState){
+            (acc,ac) => ac.next(acc)
+          }
+          getClient.setState(currState)
+          import scala.concurrent.duration._
+
+          val deadline = 5.seconds.fromNow
+          while(!isCommandDone && deadline.hasTimeLeft()){
+            wait(1)
+          }if(deadline.hasTimeLeft()){
+          Right(getClient.getState)
+        }
+          Left(getClient.getState)
+      }
+      case Some(false) => Left(getClient.getState)
+      case None => throw new IllegalArgumentException(s"Can not evaluate Command `$c`, since Partial state `$s` does not include all affected variables.")
     }
-    opcClient.setState(currState)
-    while(opcClient.getState.getOrElse("status","None").toString!="Finished"){}
-    ???
+
+
   }
 
-  override def runListOfCommands(commands: List[Command], s: StateMap): Either[StateMap, StateMap] = ???
+  override def runListOfCommands(commands: List[Command], s: StateMap): Either[StateMap, StateMap] = {
+
+
+    def runList(c:List[Command],ns: StateMap):Either[StateMap,StateMap]= {
+
+      c match {
+
+        case x::xs =>
+
+          runCommand (x, ns) match {
+            case Right (n) => runList (xs, n)
+            case Left (n) => Left (n)
+          }
+        case Nil => Right(ns)
+      }
+
+    }
+    runList(commands,s)
+
+  }
+
 }
--- src/main/scala/modelbuilding/models/MachineBuffer/MBOPCSimulate.scala
+++ src/main/scala/modelbuilding/models/MachineBuffer/MBOPCSimulate.scala
@@ -5,6 +5,7 @@ import modelbuilding.core.simulation.OPCSimulator
 
 class MBOPCSimulate extends OPCSimulator {
 
+  override val variableList= Some(List(run,execState))
 
   /**
    * Define state we are interested in. These state variables will be synced with the actual system.
@@ -12,21 +13,23 @@ class MBOPCSimulate extends OPCSimulator {
   val m1 = "m1"
   val m2 = "m2"
 
+  val run = "run"
+  val execState = "state"
 
   override val goalStates: Option[Set[StateMap]] = None
 
 
   override val guards: Map[Command, Predicate] = Map(
-    load1 -> AlwaysTrue,
-    load2 -> AlwaysTrue,
-    unload1 -> AlwaysTrue,
-    unload2 -> AlwaysTrue
+    load1 -> EQ(execState,"init"), //make guard to be such that state is initial
+    load2 -> EQ(execState,"init"),
+    unload1 -> EQ(execState,"init"),
+    unload2 -> EQ(execState,"init")
   )
 
   override val actions: Map[Command, List[Action]] = Map(
-    load1 -> List(Assign(m1, true)),
-    load2 -> List(Assign(m2,true)),
-    unload1 -> List(Assign(m1,false)),
-    unload2 -> List(Assign(m2,false))
+    load1 -> List(Assign(run, "load1")),
+    load2 -> List(Assign(run,"load2")),
+    unload1 -> List(Assign(run,"unload1")),
+    unload2 -> List(Assign(run,"unload2"))
   )
 }
