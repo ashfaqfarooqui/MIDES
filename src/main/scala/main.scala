1cd665c5d3705c2d1c9f7a2b9320ec3e88c9f94e
Ashfaq Farooqui <ashfaq.farooqui@gmail.com>
2019-10-24 10:18:54 +0200

    updating supremica jars and dependent changes
    Signed-off-by: Ashfaq Farooqui <ashfaq.farooqui@gmail.com>

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,4 +1,3 @@
-
 import grizzled.slf4j.Logging
 import modelbuilding.core.{LearningType, SUL}
 import modelbuilding.core.modeling.Model
@@ -8,7 +7,6 @@ import modelbuilding.solvers._
 import supremicastuff.SupremicaHelpers
 import supremicastuff.SupremicaHelpers._
 
-
 object ModelBuilder extends Logging {
 
   val supervisor = LearningType.SUPERVISOR
@@ -17,33 +15,75 @@ object ModelBuilder extends Logging {
   val modelName = "TestUnit"
   info(s"Starting with mode : $modelName")
 
-
   val sul: SUL = modelName match {
-    case "TestUnit" => SUL(TestUnit.TransferLine,new TestUnit.SimulateTL,Some(TLSpecifications()),supervisor,true)
-    case "CatMouse" => SUL(CatAndMouse.CatAndMouse, new CatAndMouse.SimulateCatAndMouse,None,plant,false)
-    case "CatMouseModular" =>SUL(CatAndMouseModular.CatAndMouseModular, new CatAndMouseModular.SimulateCatAndMouseModular,Some(CatAndMouseModular.CatAndMouseModularSpecification()),supervisor,false )
-    case "MachineBuffer" => SUL(MachineBuffer.MachineBuffer, new MachineBuffer.SimulateMachineBuffer,Some(MachineBuffer.MachineBufferSpecifications()),supervisor,false)
-    case "RoboticArm" => SUL(RobotArm.Arm, new RobotArm.SimulateArm(3,3),None,plant,false)
-    case "Sticks" => SUL(StickPicking.Sticks, new StickPicking.SimulateSticks(5),None,plant,false)
-    case "AGV" => SUL(AGV.Agv,new AGV.SimulateAgv,Some(AGV.AGVSpecifications()),supervisor,false)
+    case "TestUnit" =>
+      SUL(
+        TestUnit.TransferLine,
+        new TestUnit.SimulateTL,
+        Some(TLSpecifications()),
+        supervisor,
+        true
+      )
+    case "CatMouse" =>
+      SUL(
+        CatAndMouse.CatAndMouse,
+        new CatAndMouse.SimulateCatAndMouse,
+        None,
+        plant,
+        false
+      )
+    case "CatMouseModular" =>
+      SUL(
+        CatAndMouseModular.CatAndMouseModular,
+        new CatAndMouseModular.SimulateCatAndMouseModular,
+        Some(CatAndMouseModular.CatAndMouseModularSpecification()),
+        supervisor,
+        false
+      )
+    case "MachineBuffer" =>
+      SUL(
+        MachineBuffer.MachineBuffer,
+        new MachineBuffer.SimulateMachineBuffer,
+        Some(MachineBuffer.MachineBufferSpecifications()),
+        supervisor,
+        false
+      )
+    case "RoboticArm" =>
+      SUL(RobotArm.Arm, new RobotArm.SimulateArm(3, 3), None, plant, false)
+    case "Sticks" =>
+      SUL(
+        StickPicking.Sticks,
+        new StickPicking.SimulateSticks(5),
+        None,
+        plant,
+        false
+      )
+    case "AGV" =>
+      SUL(
+        AGV.Agv,
+        new AGV.SimulateAgv,
+        Some(AGV.AGVSpecifications()),
+        supervisor,
+        false
+      )
     case _ => throw new Exception("A model wasn't defined.")
   }
 
   val solver: String = "LStarSuprLearner" // "modular", "mono"
 
-  def main(args: Array[String]) : Unit= {
+  def main(args: Array[String]): Unit = {
 
     info(s"Running sul: $sul")
 
     val result = solver match {
-      case "frehage1" => new FrehagePlantBuilderWithPartialStates(sul)
-      case "frehage2" => new FrehagePlantBuilder(sul)
-      case "frehage3" => new FrehageModularSupSynthesis(sul)
-      case "monolithic" => new MonolithicSolver(sul)
+      case "frehage1"            => new FrehagePlantBuilderWithPartialStates(sul)
+      case "frehage2"            => new FrehagePlantBuilder(sul)
+      case "frehage3"            => new FrehageModularSupSynthesis(sul)
+      case "monolithic"          => new MonolithicSolver(sul)
       case "monolithicSupSolver" => new MonolithicSupSolver(sul)
-      case "modularSupSolver" => new ModularSupSolver(sul)
-      case "LStarPlantLearner" => new LStarPlantSolver(sul)
-      case "LStarSuprLearner" => new LStarSuprSolver(sul)
+      case "modularSupSolver"    => new ModularSupSolver(sul)
+      case "LStarPlantLearner"   => new LStarPlantSolver(sul)
+      case "LStarSuprLearner"    => new LStarSuprSolver(sul)
 
     }
 
@@ -53,7 +93,7 @@ object ModelBuilder extends Logging {
 
     automata.modules foreach println
     automata.modules.foreach(_.createDotFile)
-    SupremicaHelpers.exportAsSupremicaAutomata(automata, name=modelName)
+    SupremicaHelpers.exportAsSupremicaAutomata(automata, name = modelName)
   }
 
 }
