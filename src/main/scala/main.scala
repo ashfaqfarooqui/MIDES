6c8d860eb14f9d4f45340f8ede29cdbfe3cf769e
Fredrik Hagebring <fredrik@hagebring.com>
2019-11-18 10:34:40 +0100

    Added a controlled simulation model of the machine buffer that actually includes the buffer in the simulation

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -12,7 +12,7 @@ object ModelBuilder extends Logging {
   val supervisor: LearningType.Value = LearningType.SUPERVISOR
   val plant: LearningType.Value = LearningType.PLANT
 
-  val modelName: String = "AGV"
+  val modelName: String = "MachineBufferWithControl"
   info(s"Starting with mode : $modelName")
 
 
@@ -21,13 +21,14 @@ object ModelBuilder extends Logging {
     case "CatMouse" => SUL(CatAndMouse.CatAndMouse, new CatAndMouse.SimulateCatAndMouse,None,plant,false)
     case "CatMouseModular" =>SUL(CatAndMouseModular.CatAndMouseModular, new CatAndMouseModular.SimulateCatAndMouseModular,Some(CatAndMouseModular.CatAndMouseModularSpecification()),supervisor,false )
     case "MachineBuffer" => SUL(MachineBuffer.MachineBuffer, new MachineBuffer.SimulateMachineBuffer,Some(MachineBuffer.MachineBufferSpecifications()),supervisor,false)
+    case "MachineBufferWithControl" => SUL(MachineBuffer.MachineBufferWithControl, new MachineBuffer.SimulateMachineBufferWithControl,Some(MachineBuffer.MachineBufferSpecifications()),supervisor,false)
     case "RoboticArm" => SUL(RobotArm.Arm, new RobotArm.SimulateArm(3,3),None,plant,false)
     case "Sticks" => SUL(StickPicking.Sticks, new StickPicking.SimulateSticks(5),None,plant,false)
     case "AGV" => SUL(AGV.Agv,new AGV.SimulateAgv,Some(AGV.AGVSpecifications()),supervisor,false)
     case _ => throw new Exception("A model wasn't defined.")
   }
 
-  val solver: String = "frehage3" // "modular", "mono"
+  val solver: String = "frehage2" // "modular", "mono"
 
   def main(args: Array[String]) : Unit= {
 
---
+++ src/main/scala/modelbuilding/models/MachineBuffer/SimulateMachineBufferWithControl.scala
@@ -0,0 +1,28 @@
+package modelbuilding.models.MachineBuffer
+
+import modelbuilding.core._
+import modelbuilding.core.simulation.Simulator
+
+class SimulateMachineBufferWithControl extends Simulator {
+
+  val m1 = "m1"
+  val m2 = "m2"
+  val b = "b"
+
+  override val initState: StateMap = StateMap(m1->false,m2->false,b->false)
+  override val goalStates: Option[Set[StateMap]] = Some(Set(initState))
+
+  override val guards: Map[Command,Predicate] = Map(
+    load1 -> EQ(m1,false),
+    unload1 -> AND(EQ(m1,true),EQ(b,false)),
+    load2 -> AND(EQ(m2,false),EQ(b,true)),
+    unload2 -> EQ(m2,true)
+  )
+
+  override val actions: Map[Command,List[Action]] = Map(
+    load1 -> List(Toggle(m1)),
+    load2 -> List(Toggle(m2),Toggle(b)),
+    unload1 -> List(Toggle(m1),Toggle(b)),
+    unload2 -> List(Toggle(m2))
+  )
+}
